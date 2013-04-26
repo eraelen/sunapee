9af09060863e3d7242c7e02c5f40d1bfd055e9c1
eraelen <eraelen@gmail.com>
2013-04-25 22:47:54 -0400

    put ajax tweets in js file

---
+++ public/javascripts/tweet.js
@@ -0,0 +1,54 @@
+var postNewTweet = function (tweetMsg) {
+	$.ajax({
+		type: 'POST',
+		url: '/newtweet',
+		data: {msg: tweetMsg},
+		success: function(par) {
+			$('#tweetCount').text("Tweets: "+par[1]);
+			$('#tweetList').prepend(tweetToHtml(par[0]));
+			delTweet();
+		}
+		}).done(function(msg) {
+			console.log("ejs msg ",msg);
+		});
+};
+
+var deleteTweet = function (tweetID) {
+	$.ajax({
+		type: 'POST',
+		url: '/deleteTweet',
+		data: {tweetID: tweetID},
+		success: function(pair) {
+			console.log("pair[0] ",pair[0]);
+			$('#t'+pair[0]).remove();
+			$('#tweetCount').text("Tweets: "+pair[1]);
+		}
+		}).done(function(msg) { 
+			console.log("ejs msg ",msg); 
+		});
+};
+
+function newTweet() {
+		$('#newtweetSubmit').bind('click',
+		function (event) {
+			// Get tweet message
+			var data = $('textarea').val();  
+			postNewTweet(data);
+
+			// Reset input field:
+			$('textarea').val("What\'s on your mind?");
+			return false;
+		});
+	}
+	
+function delTweet() {
+	$('.delT').bind('click',
+	function (event) {
+		// Get tweet message
+		var tweetID = $(this).attr("id");  
+		deleteTweet(tweetID);
+
+		return false;
+	});
+}
+
